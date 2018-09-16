69a6630844d34c171db7427125726e1c5c37e8d7
tcitry <tcitry@gmail.com>
2018-09-16 21:19:57 +0800

    update gitlab variable

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd httpbin;docker-compose up -d --remove-orphans;cd ..
-cd gitlab;cp .env.example .env;docker-compose up -d --remove-orphans;cd ..
-cd sentry;cp .env.example .env;docker-compose up -d --remove-orphans;cd ..
+cd gitlab;docker-compose up -d --remove-orphans;cd ..
+cd sentry;docker-compose up -d --remove-orphans;cd ..
 sudo nginx -s reload
