9c6ca85746bdc1ff26fda1b208d9a7197e951d9f
tcitry <tcitry@gmail.com>
2018-09-16 19:38:43 +0800

    [fix] fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd httpbin;docker-compose up -d --remove-orphans;cd ..
-cd gitlab;cp gitlab/.env.example gitlab/.env;docker-compose up -d --remove-orphans;cd ..
-cd sentry;cp sentry/.env.example sentry/.env;docker-compose up -d --remove-orphans;cd ..
+cd gitlab;cp .env.example .env;docker-compose up -d --remove-orphans;cd ..
+cd sentry;cp .env.example .env;docker-compose up -d --remove-orphans;cd ..
 sudo nginx -s reload
